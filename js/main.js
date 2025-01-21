6cbaf1018f242eab796a9fd4f3afe272557eee2d
hoferb <33937796+hoferb@users.noreply.github.com>
2025-01-21 12:05:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,10 +19,20 @@ var osmap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
+var stadia_as = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+var statia_asd = L.tileLayer.provider(Stadia.AlidadeSmoothDark);
 
 // for using the two base maps in the layer control, I defined a baseMaps variable
 var baseMaps = {
-	"Open Street Map": osmap
+	"Open Street Map": osmap,
+	"Stadia Alidade smooth": stadia_as,
+	"Stadia Alidade smooth dark": stadia_asd
 }
 
 //
